b383eda4b56b50063f1cf22248533e0f2916f12b
Annabelle Strong <annabellestrong@gmail.com>
2018-01-28 01:26:43 -0500

    Added graphing capabilities and multi-tier mapping

--- Word.Map.py
+++ Word.Map.py
@@ -1,18 +1,26 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup, SoupStrainer
 
+import pydot
+import graphviz
+
 ####################################### FUNCTIONS ###############################################
 
 def read_html(address):
 	# Open the page
-	url = Request(address)#, headers={'User-Agent': 'Mozilla/5.0'})
+	url = Request(address, headers={'User-Agent': 'Mozilla/5.0'})
 	# Parse the HTML
 	html = urlopen(url).read()
 
 	return html
 
 def get_synonyms(word):
-	url = 'http://www.thesaurus.com/browse/' + word
+
+	split_word = word.split(' ')
+
+	word = '%20'.join(split_word)
+
+	url = 'http://www.thesaurus.com/browse/' + word + '?s=t'
 
 	# Open and parse page
 	html = read_html(url)
@@ -32,8 +40,55 @@ def get_synonyms(word):
 
 	return synonyms
 
-########################################## LOGIC ###############################################
-#word = input("What word do you want to map? ")
-word = 'athletic'
+def make_node(parent, name):
+	global graph
 
-print(get_synonyms(word))
+	new_node = pydot.Node(name)
+	graph.add_node(new_node)
+	graph.add_edge(pydot.Edge(parent, new_node))
+
+def map_synonym(word):
+	global graph
+	global recursion
+
+	synonyms = get_synonyms(word)
+
+	for i in range(len(synonyms)):
+		recursion += 1
+		synonym = synonyms[i - 1]
+		make_node(word, synonym)
+
+		if recursion <= 2:
+			map_synonym(synonym)
+
+def map_word(word):
+	global graph
+	global tree_top
+	global recursion
+
+	if tree_top == True:
+		graph.add_node(pydot.Node(word))
+
+	synonyms = get_synonyms(word)
+
+	for i in range(len(synonyms)):
+		recursion = 1
+		synonym = synonyms[i - 1]
+		make_node(word, synonym)
+
+		map_synonym(synonym)
+
+
+########################################## LOGIC ###############################################
+tree_top = True
+recursion = 0
+
+word = input("What word do you want to map? ")
+
+graph = pydot.Dot(graph_type='graph')
+
+map_word(word)
+
+graph.write_png(word + '.png')
+
+print('Mapping complete. ')
